45e7703b0a3893d51777aeda79ff90c70f904aa0
redaphid <iam@aaronherres/com>
2022-10-27 16:01:20 -0700

    finally. ugh

---
+++ get-user-events.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        New script
+// @namespace   Violentmonkey Scripts
+// @match       *://*/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 10/27/2022, 3:59:32 PM
+// ==/UserScript==
+console.log('hi')
